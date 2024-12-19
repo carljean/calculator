5784f1ad9a0df4bbffcd84ff43b2bb08cb4d6fda
Carl Jean <66816390+carljean@users.noreply.github.com>
2024-12-19 11:54:38 -0500

    Add functions to populate the value of the buttons into the screen

--- calculator.js
+++ calculator.js
@@ -15,10 +15,18 @@ function divide(a, b) {
 
     return a / b;
 }
-
+// ============= VARIABLES =================
 let operand1 = '';
 let operand2 = '';
 let operator = '';
+const numKeys = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('#clear');
+const deleteBtn = document.querySelector('#delete');
+const equalBtn = document.querySelector('#equal');
+let screen_display = document.querySelector('.screen div');
+let screen_result = document.querySelector('.screen div + div');
+// =========================================
 
 function operate(operator, a, b) {
     if(operator === '+') return add(a, b);
@@ -30,4 +38,45 @@ function operate(operator, a, b) {
     if(operator === '/') return divide(a, b);
 }
 
-// console.log(operate('/', 10, 0));
+function write() {
+    for(let i = 0; i < numKeys.length; i++) {
+        numKeys[i].addEventListener('click', function() {
+            if(screen_result.textContent == '0') {
+                screen_result.textContent = '';
+            }
+
+            if(!(screen_result.textContent.includes('.') && numKeys[i].textContent === '.'))
+                screen_result.textContent += numKeys[i].textContent;
+        });
+    }
+
+    for(let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', function() {
+            screen_display.textContent = screen_result.textContent;
+            screen_display.textContent += operators[i].textContent;
+            screen_result.textContent = '';
+        });
+    }
+}
+
+function erase() {
+    // remove screen content one by one
+    const arr = screen_result.textContent.split("");
+    arr.pop();
+    screen_result.textContent = arr.join("");
+}
+
+function clear() {
+    screen_display.textContent = '';
+    screen_result.textContent = 0;
+}
+
+write();
+
+clearBtn.addEventListener('click', function() {
+    clear();
+});
+
+deleteBtn.addEventListener('click', function() {
+    erase();
+});
